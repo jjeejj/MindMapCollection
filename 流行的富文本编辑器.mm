77281cf511c48c83142a31fb5797937e99aa9159
jjeejj <wenjunjiang1993@163.com>
2018-10-07 09:52:42 +0800

    流行的富文本编辑器

---
+++ 流行的富文本编辑器.mm
@@ -0,0 +1,66 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1538834594178" ID="bovzc59tzk80" MODIFIED="1538834594178" MODIFIED="1538834594178" TEXT="流行的富文本编辑器">
+<node CREATED="1538835372275" ID="bovzm2q4wuo0" MODIFIED="1538835372275" MODIFIED="1538835372275" TEXT="tinymce">
+<richcontent TYPE="NODE"><html><head></head><body>
+<img src="https://img.shields.io/github/stars/tinymce/tinymce.svg?style=social" width="82" height="20"/>
+</body></html></richcontent>
+</node>
+
+<node CREATED="1538835994700" ID="bovzu0nwfxk0" MODIFIED="1538835994700" MODIFIED="1538835994700" TEXT="medium-editor">
+<richcontent TYPE="NODE"><html><head></head><body>
+<img src="https://img.shields.io/github/stars/yabwe/medium-editor.svg?style=social" width="90" height="20"/>
+</body></html></richcontent>
+</node>
+
+<node CREATED="1538835994700" ID="bovzu0nwfxk0" MODIFIED="1538835994700" MODIFIED="1538835994700" TEXT="ckeditor5">
+<richcontent TYPE="NODE"><html><head></head><body>
+<img src="https://img.shields.io/github/stars/ckeditor/ckeditor5.svg?style=social" width="82" height="20"/>
+</body></html></richcontent>
+</node>
+
+<node CREATED="1538835994700" ID="bovzu0nwfxk0" MODIFIED="1538835994700" MODIFIED="1538835994700" TEXT="simditor">
+<richcontent TYPE="NODE"><html><head></head><body>
+<img src="https://img.shields.io/github/stars/mycolorway/simditor.svg?style=social" width="82" height="20"/>
+</body></html></richcontent>
+</node>
+
+<node CREATED="1538835994700" ID="bovzu0nwfxk0" MODIFIED="1538835994700" MODIFIED="1538835994700" TEXT="summernote">
+<richcontent TYPE="NODE"><html><head></head><body>
+<img src="https://img.shields.io/github/stars/summernote/summernote.svg?style=social" width="82" height="20"/>
+</body></html></richcontent>
+</node>
+
+<node CREATED="1538835372275" ID="bovzm2q4wuo0" MODIFIED="1538835372275" MODIFIED="1538835372275" TEXT="wangEditor">
+<richcontent TYPE="NODE"><html><head></head><body>
+<img src="https://img.shields.io/github/stars/wangfupeng1988/wangEditor.svg?style=social" width="82" height="20"/>
+</body></html></richcontent>
+</node>
+
+<node CREATED="1538835372275" ID="bovzm2q4wuo0" MODIFIED="1538835372275" MODIFIED="1538835372275" TEXT="kindeditor">
+<richcontent TYPE="NODE"><html><head></head><body>
+<img src="https://img.shields.io/github/stars/kindsoft/kindeditor.svg?style=social" width="82" height="20"/>
+</body></html></richcontent>
+</node>
+
+<node CREATED="1538835372275" ID="bovzm2q4wuo0" MODIFIED="1538835372275" MODIFIED="1538835372275" TEXT="Trumbowyg">
+<richcontent TYPE="NODE"><html><head></head><body>
+<img src="https://img.shields.io/github/stars/Alex-D/Trumbowyg.svg?style=social" width="82" height="20"/>
+</body></html></richcontent>
+</node>
+
+<node CREATED="1538835372275" ID="bovzm2q4wuo0" MODIFIED="1538835372275" MODIFIED="1538835372275" TEXT="draft-js
+React 使用的">
+<richcontent TYPE="NODE"><html><head></head><body>
+<img src="https://img.shields.io/github/stars/facebook/draft-js.svg?style=social" width="90" height="20"/>
+</body></html></richcontent>
+</node>
+
+<node CREATED="1538835372275" ID="bovzm2q4wuo0" MODIFIED="1538835372275" MODIFIED="1538835372275" TEXT="vue-quill-editor
+Vue使用的">
+<richcontent TYPE="NODE"><html><head></head><body>
+<img src="https://img.shields.io/github/stars/surmon-china/vue-quill-editor.svg?style=social" width="82" height="20"/>
+</body></html></richcontent>
+</node>
+</node>
+</map>
